e1a3c66c3fe3b9cc48f211bb61a7c9d976f5ccff
Dylan <dylan.t.styman@student.uts.edu.au>
2022-05-19 12:37:34 +1000

    Changes for ORDER (KeyFeat3) - Version 0 - DS
    Created following JSP pages, DB and initial content for Key Features 3:
    - Cart Summary Page
    - Order Summary Page
    - Order Form Page
    - Order Confirm Page
    - Order Outcome Page
    - Order Database

--- IoTBay/src/main/java/model/orderModel.java
+++ IoTBay/src/main/java/model/orderModel.java
@@ -1,15 +1,11 @@
 package model;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author kingsleybyrne
+ * edited by Dylan S.
  */
+
 public interface orderModel {
     void addOrder();
     void cancelOrder();
